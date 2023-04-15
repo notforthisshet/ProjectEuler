deac5066fe685112a1f5e95aeb4136dc02a24d22
notforthisshet <130784496+notforthisshet@users.noreply.github.com>
2023-04-15 23:17:39 +0300

    Add files via upload

---
+++ ProjectEuler1.py
@@ -0,0 +1,3 @@
+numbers = [x for x in range(1, 1001) if x % 3 == 0 or x % 5 == 0]  # creating a list with numbers divided by 3 or 5
+print(sum(numbers))  # printing the sum of numbers
+# Output: 234168
